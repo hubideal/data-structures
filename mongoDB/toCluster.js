8e4475c2f9516012bff5af7e88d3b2c8dcf619d6
Steven Hubbard <hubbs654@newschool.edu>
2017-11-17 00:35:48 +0000

    changes to env key

--- mongoDB/toCluster.js
+++ mongoDB/toCluster.js
@@ -4,7 +4,7 @@ var meetings = JSON.parse(fs.readFileSync('aaMeetingCluster.JSON'));
 
 // Connection URL
 // var url = 'mongodb://' + process.env.IP + ':27017/aa';
-var url = 'mongodb://hubbs654:V2hYRNpJ8VNAMai6@cluster0-shard-00-00-rzdcl.mongodb.net:27017,cluster0-shard-00-01-rzdcl.mongodb.net:27017,cluster0-shard-00-02-rzdcl.mongodb.net:27017/steve?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin:27017/aa';
+ var url = 'mongodb://' + process.env.IP + ':27017/aa';
 
 // Retrieve
 var MongoClient = require('mongodb').MongoClient; // npm install mongodb
