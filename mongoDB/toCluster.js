45a76697b9ab8d31689e30589dc634edcac88db7
Steven Hubbard <hubbs654@newschool.edu>
2017-11-17 03:19:44 +0000

    change to ATLAS

--- mongoDB/toCluster.js
+++ mongoDB/toCluster.js
@@ -4,7 +4,7 @@ var meetings = JSON.parse(fs.readFileSync('aaMeetingCluster.JSON'));
 
 // Connection URL
 // var url = 'mongodb://' + process.env.IP + ':27017/aa';
- var url = 'mongodb://' + process.env.IP + ':27017/aa';
+ var url = process.env.ATLAS;
 
 // Retrieve
 var MongoClient = require('mongodb').MongoClient; // npm install mongodb
