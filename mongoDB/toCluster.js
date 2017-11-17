bf24ff256a639e479e2d858fa201d08be1d758b2
Steven Hubbard <hubbs654@newschool.edu>
2017-11-17 04:35:13 +0000

    removed /

--- mongoDB/toCluster.js
+++ mongoDB/toCluster.js
@@ -2,20 +2,18 @@ var fs = require('fs');
 
 var meetings = JSON.parse(fs.readFileSync('aaMeetingCluster.JSON'));
 
-// Connection URL
-// var url = 'mongodb://' + process.env.IP + ':27017/aa';
+
  var url = process.env.ATLAS;
 
-// Retrieve
-var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+
+var MongoClient = require('mongodb').MongoClient; 
 
 MongoClient.connect(url, function(err, db) {
     if (err) {return console.dir(err);}
 
     var collection = db.collection('meetings');
 
-    // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
     collection.insert(meetings);
     db.close();
 
-}); //MongoClient.connect
+}); 
