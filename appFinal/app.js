7a8f842ae04e40a8e63c270fb0a976bbdfd5254a
Steven Hubbard <hubbs654@newschool.edu>
2017-12-05 00:57:54 +0000

    updating the final app

--- appFinal/app.js
+++ appFinal/app.js
@@ -1,38 +1,41 @@
-var express = require('express'),
+require('express'),
     app = express();
 var fs = require('fs');
+
+var moment = require('moment-timezone'); // npm install moment-timezone
 
 // Postgres
 const { Pool } = require('pg');
 var db_credentials = new Object();
-db_credentials.user = 'datahub';
+db_credentials.user = 'hubdata';
 db_credentials.host = process.env.AWSRDS_EP;
-db_credentials.database = 'doralee';
+db_credentials.database = 'pugSensor';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
 // Mongo
-var collName = 'meetings';
+var collName = 'newMeetings'; //changed from "meetings"
 var MongoClient = require('mongodb').MongoClient;
 var url = process.env.ATLAS;
 
 // HTML wrappers for AA data
 var index1 = fs.readFileSync("index1.txt");
 var index3 = fs.readFileSync("index3.txt");
+console.log(url);
 
 app.get('/', function(req, res) {
     // Connect to the AWS RDS Postgres database
     const client = new Pool(db_credentials);
 
     // SQL query
-    var q = `SELECT EXTRACT(DAY FROM timehalltilt AT TIME ZONE 'America/New_York') as sensorday, 
-             EXTRACT(MONTH FROM timehalltilt AT TIME ZONE 'America/New_York') as sensormonth, 
+    var q = `SELECT EXTRACT(DAY FROM timehalltilt AT TIME ZONE 'America/New_York') as sensorday,
+             EXTRACT(MONTH FROM timehalltilt AT TIME ZONE 'America/New_York') as sensormonth,
              count(*) as num_obs,
              count(tilt) FILTER (WHERE tilt = TRUE) AS puginside,
-             count(hall) FILTER (WHERE hall = TRUE) AS puginside
+             count(hall) FILTER (WHERE hall = TRUE) AS puglatch
              FROM pugdata
              GROUP BY sensormonth, sensorday;`;
-             
+
     client.connect();
     client.query(q, (qerr, qres) => {
         res.send(qres.rows);
@@ -47,17 +50,18 @@ app.get('/aa', function(req, res) {
         if (err) {return console.dir(err);}
         var dayWeek = ['Sundays', 'Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays'];
         var dateTimeNow = new Date();
-        var today = dayWeek[dateTimeNow.getDay()];
+        var today = dayWeek[moment.tz(new Date(), "America/New_York").days()];
         var tomorrow;
         if (today == 'Saturdays') {tomorrow = "Sundays";}
-        else {tomorrow = dayWeek[dateTimeNow.getDay()+1]}
-        var hour = dateTimeNow.getHours();
+        else {tomorrow = dayWeek[(moment.tz(new Date(), "America/New_York").days())+1]}
+        var hour = moment.tz(new Date(), "America/New_York").hours();
+        console.log(tomorrow);
 
         var collection = db.collection(collName);
-    
+
         collection.aggregate([ // start of aggregation pipeline
             // match by day and time
-            { $match : 
+            { $match :
                 { $or : [
                     { $and: [
                         { meetingDay : today } , { startHourQ : { $gte: hour } }
@@ -67,11 +71,11 @@ app.get('/aa', function(req, res) {
                     ]}
                 ]}
             },
-            
+
             // group by meeting group
             { $group : { _id : {
                 latLong : "$latLong",
-                meetingName : "$meetingTitle",
+                meetingName : "$newTitle",
                 meetingAddress1 : "$streetAddress",
                 meetingAddress2 : "$roomFloor",
                 googleAddress : "$googleAdd",
@@ -79,23 +83,23 @@ app.get('/aa', function(req, res) {
                 meetingWheelchair : "$accessibility",
                 },
                     meetingDay : { $push : "$meetingDay" },
-                    meetingStartTime : { $push : "$startTimeWeb" }, 
+                    meetingStartTime : { $push : "$startTimeWeb" },
                     meetingType : { $push : "$meetType" },
                     meetingSpecial: { $push: "$specialInterest" },
             }
             },
-            
+
             // group meeting groups by latLong
             {
-                $group : { _id : { 
+                $group : { _id : {
                     latLong : "$_id.latLong"},
                     meetingGroups : { $push : {groupInfo : "$_id", meetingDay : "$meetingDay", meetingStartTime : "$meetingStartTime", meetingType : "$meetingType" }}
                 }
             }
-        
+
             ]).toArray(function(err, docs) { // end of aggregation pipeline
             if (err) {console.log(err)}
-            
+
             else {
                 res.writeHead(200, {'content-type': 'text/html'});
                 res.write(index1);
@@ -105,7 +109,7 @@ app.get('/aa', function(req, res) {
             db.close();
         });
     });
-    
+
 });
 
 // app.listen(process.env.PORT, function() {
